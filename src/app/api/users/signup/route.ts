aef3845ff21d4903541eb5faa23f5b661562e2b7
amyadavv <amityadav010213@gmail.com>
2025-06-06 18:27:08 +0530

    Completed

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,6 +2,7 @@ import { connect } from "@/dbconfig/dbConfig";
 import User from "@/models/userModels";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
 
 connect();
 
@@ -29,6 +30,9 @@ export async function POST(request: NextRequest) {
         })
         const savedUser = await newUser.save();
         console.log(savedUser);
+
+        //send verification email
+        await sendEmail({email, emailType: "VERIFY", userId: savedUser._id})
 
         return NextResponse.json({
             message: "User created successfully",
